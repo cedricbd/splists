67f90ba6bb9d76bd7a152516d46f20d9c6a1bba4
cedricbd <=>
2019-01-11 12:37:10 +0100

    fix path and correct

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
-<?php include ('../partials/header.php'); ?>
+<?php
+session_start();
+$_SESSION['page'] = 'index';
+?>
+
+<?php include ('partials/header.php'); ?>
 
     <!-- contenu de index.php, board.php, login.php, signup.php -->
 
-<?php include ('../partials/footer.php'); ?>  
+<?php include ('partials/footer.php'); ?>  
--- view/board.php
+++ view/board.php
@@ -1,3 +1,8 @@
+<?php
+session_start();
+$_SESSION['page'] = 'index';
+?>
+
 <?php include ('../partials/header.php'); ?>
 
     <!-- contenu de index.php, board.php, login.php, signup.php -->
